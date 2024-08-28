05e4ed766caecab11dc8e2a0b6d405404bd2b444
Catavy <tatiana_casanova@outlook.com>
2024-08-27 21:43:13 -0500

    subir proyecto web 2024

---
+++ js/saludo.js
@@ -0,0 +1,8 @@
+let contenido = document.getElementById("mensaje");
+if (!localStorage.nom) {
+    let nombre = prompt("Digita tu nombre");
+    localStorage.nom = nombre;
+    contenido.innerHTML = "Hola " + nombre;
+} else {
+    contenido.innerHTML = "Hola IA  " + localStorage.nom;
+}
